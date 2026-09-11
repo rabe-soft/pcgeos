4e28ef2d08b25107e7de3e00edd2593091f1b3f0
RainerB <r.bettsteller@web.de>
2026-09-11 20:25:35 +0200

    Move dither back to bmptools. Add replace by nearest color

--- Installed/Library/RabeSoft/bmptools/dependencies.mk
+++ Installed/Library/RabeSoft/bmptools/dependencies.mk
@@ -82,8 +82,8 @@ disptool.eobj: geos.h Ansi/string.h heap.h geode.h resource.h ec.h \
                 win.h uDialog.h Objects/gInterC.h Objects/Text/tCommon.h \
                 stylesh.h driver.h thread.h print.h Internal/spoolInt.h \
                 serialDr.h parallDr.h hugearr.h fileEnum.h
-dither.obj \
-dither.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
+floyd.obj \
+floyd.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
                 Objects/inputC.goh Objects/clipbrd.goh \
                 Objects/uiInputC.goh iacp.goh Objects/winC.goh \
                 Objects/gProcC.goh alb.goh Objects/processC.goh \
@@ -103,8 +103,8 @@ dither.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
                 Objects/helpCC.goh Ansi/stdlib.h Ansi/stdio.h \
                 Ansi/string.h Objects/colorC.goh bmptools.goh \
                 Code/rtools.goh
-dither.obj \
-dither.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
+floyd.obj \
+floyd.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
                 graphics.h fontID.h font.h color.h gstring.h timer.h vm.h \
                 dbase.h localize.h Ansi/ctype.h timedate.h file.h \
                 sllang.h system.h geoworks.h chunkarr.h Objects/helpCC.h \
@@ -197,6 +197,36 @@ moretool.eobj: geos.h Ansi/string.h heap.h geode.h resource.h ec.h \
                 win.h uDialog.h Objects/gInterC.h Objects/Text/tCommon.h \
                 stylesh.h driver.h thread.h print.h Internal/spoolInt.h \
                 serialDr.h parallDr.h hugearr.h fileEnum.h
+replace.obj \
+replace.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
+                Objects/inputC.goh Objects/clipbrd.goh \
+                Objects/uiInputC.goh iacp.goh Objects/winC.goh \
+                Objects/gProcC.goh alb.goh Objects/processC.goh \
+                Objects/visC.goh Objects/vCompC.goh Objects/vCntC.goh \
+                Objects/gAppC.goh Objects/genC.goh Objects/gInterC.goh \
+                Objects/gPrimC.goh Objects/gDispC.goh Objects/gTrigC.goh \
+                Objects/gViewC.goh Objects/gTextC.goh Objects/vTextC.goh \
+                Objects/gCtrlC.goh gcnlist.goh spool.goh \
+                Objects/gFSelC.goh Objects/gGlyphC.goh \
+                Objects/gDocCtrl.goh Objects/gDocGrpC.goh \
+                Objects/gDocC.goh Objects/gContC.goh Objects/gDCtrlC.goh \
+                Objects/gEditCC.goh Objects/gBoolGC.goh \
+                Objects/gItemGC.goh Objects/gDListC.goh \
+                Objects/gItemC.goh Objects/gBoolC.goh \
+                Objects/gGadgetC.goh Objects/gToolCC.goh \
+                Objects/gValueC.goh Objects/gToolGC.goh \
+                Objects/helpCC.goh Ansi/stdlib.h Ansi/stdio.h \
+                Ansi/string.h Objects/colorC.goh bmptools.goh \
+                Code/rtools.goh
+replace.obj \
+replace.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
+                graphics.h fontID.h font.h color.h gstring.h timer.h vm.h \
+                dbase.h localize.h Ansi/ctype.h timedate.h file.h \
+                sllang.h system.h geoworks.h chunkarr.h Objects/helpCC.h \
+                disk.h drive.h input.h char.h hwr.h win.h uDialog.h \
+                Objects/gInterC.h Objects/Text/tCommon.h stylesh.h \
+                driver.h thread.h print.h Internal/spoolInt.h serialDr.h \
+                parallDr.h hugearr.h fileEnum.h
 transform.obj \
 transform.eobj: stdlib.h bmptools.goh stdapp.goh object.goh ui.goh \
                 Objects/metaC.goh Objects/inputC.goh Objects/clipbrd.goh \
